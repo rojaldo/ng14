bf7aff3a5bf3731268010d6f0d2ab6c4231d712b
Daniel <danielrojo@gmail.com>
2022-10-11 15:16:36 +0200

    bootstrap added

---
+++ src/app/components/heroes/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+
+  heroName = '';
+
+  addHero(): void {
+    if (this.heroName.trim().length === 0) {
+      return;
+    }
+    this.heroes.push(this.heroName);
+    this.heroName = '';
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
